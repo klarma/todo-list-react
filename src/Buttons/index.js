0e3c68b5aba5b54604c610eb24075f88524d36ec
Katarzyna Napierala <k_napierala@op.pl>
2021-06-05 14:22:23 +0200

    span changed into div as a container

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,22 +2,21 @@ import React from "react";
 import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
-    <span className="section__span">
+    <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button className="section__button">
+                <button className="buttons__button">
                     {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
                 </button>
                 <button
-                    className="section__button"
+                    className="buttons__button"
                     disabled={tasks.every(({ done }) => done)}
                 >
                     Ukończ wszystkie
                 </button>
             </>
         )}
-
-    </span>
+    </div>
 );
 
 export default Buttons;
