0432eb1a7d6ed95028464dca3485cd98c38ca9a7
Katarzyna Napierala <k_napierala@op.pl>
2021-06-07 14:50:51 +0200

    className tasksList changed to tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,33 +1,33 @@
 import "./style.css";
 
 const Tasks = ({ tasks, hideDoneTasks }) => (
-    <ul className="tasksList">
+    <ul className="tasks">
         {
             tasks.map(task => (
                 <li
                     key={task.id}
-                    className={`tasksList__item${task.done && hideDoneTasks
-                        ? " taskList__item--hide"
+                    className={`tasks__item${task.done && hideDoneTasks
+                        ? " tasks__item--hide"
                         : ""}`
                     }
                 >
                     <button
-                        className="tasksList__button tasksList__button--done">
+                        className="tasks__button tasks__button--done">
                         {task.done
                             ? <i class='fas fa-check'></i>
                             : ""}
                     </button>
                     <span
                         className={
-                            `tasksList__spanTask${task.done
-                                ? " tasksList__spanTask--done"
+                            `tasks__spanTask${task.done
+                                ? " tasks__spanTask--done"
                                 : ""}`
                         }
                     >
                         {task.content}
                     </span>
                     <button
-                        className="tasksList__button">
+                        className="tasks__button">
                         <i className="fas fa-trash-alt"></i>
                     </button>
                 </li>
