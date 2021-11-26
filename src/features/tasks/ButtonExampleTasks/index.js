56d3892631f7d1e66c38d146a83e487dd0d1f676
Katarzyna Napierala <k_napierala@op.pl>
2021-11-26 10:17:13 +0100

    Wrapper added in ButtonExampleTasks

--- src/features/tasks/ButtonExampleTasks/index.js
+++ src/features/tasks/ButtonExampleTasks/index.js
@@ -1,23 +1,26 @@
 import { useSelector, useDispatch } from "react-redux";
 import { fetchExampleTasks, selectButtonState } from "../tasksSlice";
-import { Button } from "../styledButton";
+import { Button, Wrapper } from "../styledButton";
 
 const ButtonExampleTasks = () => {
     const buttonState = useSelector(selectButtonState);
     const dispatch = useDispatch();
 
     return (
-        buttonState ?
-            (
-                <Button onClick={() => dispatch(fetchExampleTasks())}>
-                    Pobierz przykładowe zadania
-                </Button>
-            ) :
-            (
-                <Button disabled>
-                    Ładowanie
-                </Button>
-            )
+        <Wrapper>
+            {buttonState ?
+                (
+                    <Button onClick={() => dispatch(fetchExampleTasks())}>
+                        Pobierz przykładowe zadania
+                    </Button>
+                ) :
+                (
+                    <Button disabled>
+                        Ładowanie
+                    </Button>
+                )
+            }
+        </Wrapper>
     );
 };
 
